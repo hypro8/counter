44861f513c6c7be9b3608dc77f0df0730e98bf98
Yang <hao.frank@gmail.com>
2022-02-09 16:05:20 +0000

    fix mobile 001

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
 import MainCounter from "./components/MainCounter";
 import MainHeader from "./components/Header";
-import SettingBar from "./components/Sidebar";
-import { useState } from "react";
 
 function App() {
-  const [sideOpen, setSideOpen] = useState(true);
   return (
     <main className="main-section">
       <MainHeader />
-      <MainCounter side={sideOpen} setSide={setSideOpen} />
-      {/* <SettingBar side={sideOpen} /> */}
+      <MainCounter />
     </main>
   );
 }
